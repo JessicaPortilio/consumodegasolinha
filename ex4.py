36ad2d2f3e4cf9f8135313be69d58488e8ef02b0
JessicaPortilio <jessicaket91@gmail.com>
2020-07-13 16:36:26 -0300

    first commit

---
+++ ex4.py
@@ -0,0 +1,4 @@
+km_percorridos = int(input("Informe o número de quilômetros percorridos: "))
+litros_consumidos = int(input("Informe o número de litros de gasolina consumidos: "))
+consumo = km_percorridos / litros_consumidos
+print("O consumo do carro foi de %.2f" %consumo + " quilômetros por litro.")
